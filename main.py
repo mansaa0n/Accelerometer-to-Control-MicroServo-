7d4242daa3936c08d9470f985b923969add831d3
Ahmed Almansor <ahmed.almansor@hotmail.com>
2018-09-08 02:05:14 -0700

    All in one...

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py -- put your code here!
+import pyb   # importing the board
+# move servo based on the movement of the board
+from pyb import Servo
+accel = pyb.Accel()     # assigning the sensor to act as movement detector…
+ss = Servo(1) # servo on position 1 (X1, VIN, GND)
+while True:     # while loop, looping while switch is not pressed…
+    if accel.x() > 5:
+        ss.angle(90)
+    elif accel.x() < -5:
+        ss.angle(-90)
+    elif accel.x() == 0:
+        ss.angle(0)
